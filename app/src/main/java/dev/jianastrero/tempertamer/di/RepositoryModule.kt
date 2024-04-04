08ead9d73cd8be839f5f4ebe9d54d41941a9adec
Jian James Astrero <jianjamesastrero@gmail.com>
2024-04-05 04:54:41 +0800

    🧑‍💻 Create repository module

---
+++ app/src/main/java/dev/jianastrero/tempertamer/di/RepositoryModule.kt
@@ -0,0 +1,23 @@
+package dev.jianastrero.tempertamer.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dev.jianastrero.tempertamer.repository.level.ILevelRepository
+import dev.jianastrero.tempertamer.repository.level.LevelAPIRepository
+import javax.inject.Named
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class RepositoryModule {
+    @Singleton
+    @Provides
+    @Named("level_api")
+    fun provideLevelAPIRepository(
+        @ApplicationContext context: Context
+    ): ILevelRepository = LevelAPIRepository(context)
+}
