880647099d4d5a0785a415f072dbe49bfae131e3
Jian James Astrero <jianjamesastrero@gmail.com>
2024-04-05 00:24:09 +0800

    🧑‍💻 Create AppDatabase

---
+++ app/src/main/java/dev/jianastrero/tempertamer/db/AppDatabase.kt
@@ -0,0 +1,26 @@
+package dev.jianastrero.tempertamer.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import dev.jianastrero.tempertamer.dao.LevelDao
+import dev.jianastrero.tempertamer.domain.entity.Activity
+import dev.jianastrero.tempertamer.domain.entity.Details
+import dev.jianastrero.tempertamer.domain.entity.File
+import dev.jianastrero.tempertamer.domain.entity.Icon
+import dev.jianastrero.tempertamer.domain.entity.Level
+import dev.jianastrero.tempertamer.domain.entity.LockedIcon
+
+@Database(
+    entities = [
+        Level::class,
+        Activity::class,
+        Icon::class,
+        LockedIcon::class,
+        File::class,
+        Details::class
+    ],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract class levelDao(): LevelDao
+}
