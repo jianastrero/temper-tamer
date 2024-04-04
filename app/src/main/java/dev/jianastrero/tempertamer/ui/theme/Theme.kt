269c44ed5a3d52927bf1fc20650bde8bd50b29ac
Jian James Astrero <jianjamesastrero@gmail.com>
2024-04-05 04:55:06 +0800

    🧑‍💻 Update theme to always start with a surface composable

--- app/src/main/java/dev/jianastrero/tempertamer/ui/theme/Theme.kt
+++ app/src/main/java/dev/jianastrero/tempertamer/ui/theme/Theme.kt
@@ -3,13 +3,16 @@ package dev.jianastrero.tempertamer.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -65,6 +68,11 @@ fun TemperTamerTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
-    )
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+            content = content
+        )
+    }
 }
