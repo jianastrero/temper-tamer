93417ab597bd006eb7da8dfa1a6f6edbecd24eda
Jian James Astrero <jianjamesastrero@gmail.com>
2024-04-05 00:35:30 +0800

    🧑‍💻 Create TemperTamerApplication

---
+++ app/src/main/java/dev/jianastrero/tempertamer/TemperTamerApplication.kt
@@ -0,0 +1,8 @@
+package dev.jianastrero.tempertamer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TemperTamerApplication : Application() {
+}
