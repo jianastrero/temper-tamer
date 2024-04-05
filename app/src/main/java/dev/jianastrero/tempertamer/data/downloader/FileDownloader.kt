213491e68fa69ccd615ae5c5c58f4b06d4741b18
Jian James Astrero <jianjamesastrero@gmail.com>
2024-04-06 06:46:31 +0800

    🧑‍💻 Create FileDownloader

---
+++ app/src/main/java/dev/jianastrero/tempertamer/data/downloader/FileDownloader.kt
@@ -0,0 +1,49 @@
+package dev.jianastrero.tempertamer.data.downloader
+
+import android.content.Context
+import android.util.Log
+import java.io.File
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+object FileDownloader {
+
+    suspend fun download(context: Context, url: String, filename: String): Boolean =
+        withContext(Dispatchers.IO) {
+            val fullUrl = if (url.startsWith("http")) url else "https:$url"
+            var connection: HttpURLConnection? = null
+            var result = false
+
+            try {
+                connection = URL(fullUrl).openConnection() as HttpURLConnection
+                connection.connect()
+
+                val statusCode = connection.responseCode
+                if (statusCode != HttpURLConnection.HTTP_OK) {
+                    // handle error here
+                    Log.d("JIANDDEBUG", "Connection error: $statusCode")
+                    connection.disconnect()
+                    return@withContext false
+                }
+
+                connection.inputStream.use { inputStream ->
+                    File(context.filesDir, filename).also {
+                        it.createNewFile()
+                    }.outputStream().use { outputStream ->
+                        outputStream.write(inputStream.readBytes())
+                    }
+                }
+                result = true
+            } catch (e: Exception) {
+                Log.d("JIANDDEBUG", "e.message: ${e.message}")
+                e.printStackTrace()
+            } finally {
+                connection?.disconnect()
+            }
+
+            result
+        }
+
+}
