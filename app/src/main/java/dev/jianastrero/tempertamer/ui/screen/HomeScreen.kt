a210db2fbcd7a57556974b58b18ca57d8a0d02a0
Jian James Astrero <jianjamesastrero@gmail.com>
2024-04-06 08:18:40 +0800

    🧑‍💻 Add Journey footer

--- app/src/main/java/dev/jianastrero/tempertamer/ui/screen/HomeScreen.kt
+++ app/src/main/java/dev/jianastrero/tempertamer/ui/screen/HomeScreen.kt
@@ -1,23 +1,37 @@
 package dev.jianastrero.tempertamer.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.jianastrero.tempertamer.R
 import dev.jianastrero.tempertamer.ui.component.DayTabs
 import dev.jianastrero.tempertamer.ui.component.Header
 import dev.jianastrero.tempertamer.ui.component.LevelItem
+import dev.jianastrero.tempertamer.ui.theme.Divider
 import dev.jianastrero.tempertamer.viewmodel.main.HomeViewModel
 import kotlinx.coroutines.launch
 
@@ -69,6 +83,33 @@ fun HomeScreen(
                 )
             }
             item { Spacer(modifier = Modifier.height(40.dp)) }
+        }
+        Spacer(
+            modifier = Modifier
+                .background(Divider)
+                .fillMaxWidth()
+                .height(1.dp)
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(11.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 12.dp, bottom = 16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_journey),
+                contentDescription = null,
+                modifier = Modifier.size(24.dp)
+            )
+            Text(
+                text = "Journey",
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.W500,
+                lineHeight = 16.sp,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
