11127a771009603450cc0dd66a9142aa9612cf93
Jian James Astrero <jianjamesastrero@gmail.com>
2024-04-06 14:34:34 +0800

    🧑‍💻 Run repository suspended functions on IO Dispatchers

--- app/src/main/java/dev/jianastrero/tempertamer/data/repository/level/LevelAPIRepository.kt
+++ app/src/main/java/dev/jianastrero/tempertamer/data/repository/level/LevelAPIRepository.kt
@@ -6,6 +6,8 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.jianastrero.tempertamer.domain.entity.Level
 import dev.jianastrero.tempertamer.domain.response.HomeResponse
 import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
@@ -14,10 +16,10 @@ class LevelAPIRepository @Inject constructor(
     @ApplicationContext private val context: Context
 ) : ILevelRepository {
     @OptIn(ExperimentalSerializationApi::class)
-    override suspend fun getLevels(): List<Level> {
+    override suspend fun getLevels(): List<Level> = withContext(Dispatchers.IO) {
         val response = Json.decodeFromStream<HomeResponse>(context.assets.open("home.json"))
         Log.d("JIANDDEBUG", "response: $response")
-        return response.levels
+        response.levels
     }
 
     override suspend fun insertAll(levels: List<Level>) {
--- app/src/main/java/dev/jianastrero/tempertamer/data/repository/level/LevelDBRepository.kt
+++ app/src/main/java/dev/jianastrero/tempertamer/data/repository/level/LevelDBRepository.kt
@@ -3,16 +3,21 @@ package dev.jianastrero.tempertamer.data.repository.level
 import dev.jianastrero.tempertamer.data.dao.LevelDao
 import dev.jianastrero.tempertamer.domain.entity.Level
 import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class LevelDBRepository @Inject constructor(
     private val levelDao: LevelDao
 ) : ILevelRepository {
-    override suspend fun getLevels(): List<Level> =
+    override suspend fun getLevels(): List<Level> = withContext(Dispatchers.IO) {
         levelDao.getAll()
+    }
 
-    override suspend fun insertAll(levels: List<Level>) =
+    override suspend fun insertAll(levels: List<Level>) = withContext(Dispatchers.IO) {
         levelDao.insertAll(levels)
+    }
 
-    override suspend fun deleteAll() =
+    override suspend fun deleteAll() = withContext(Dispatchers.IO) {
         levelDao.deleteAll()
+    }
 }
