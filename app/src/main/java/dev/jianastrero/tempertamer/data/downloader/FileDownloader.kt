7ab612eb7104d0b443dbf6ab93dd43415a072d1d
Jian James Astrero <jianjamesastrero@gmail.com>
2024-04-06 11:28:56 +0800

    🧑‍💻 Create downloadPdfAsImageBitmap on FileDownloader

--- app/src/main/java/dev/jianastrero/tempertamer/data/downloader/FileDownloader.kt
+++ app/src/main/java/dev/jianastrero/tempertamer/data/downloader/FileDownloader.kt
@@ -2,6 +2,10 @@ package dev.jianastrero.tempertamer.data.downloader
 
 import android.content.Context
 import android.util.Log
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import dev.jianastrero.tempertamer.data.cache.BitmapCache
+import dev.jianastrero.tempertamer.data.converter.PdfToBitmap
 import java.io.File
 import java.net.HttpURLConnection
 import java.net.URL
@@ -10,46 +14,68 @@ import kotlinx.coroutines.withContext
 
 object FileDownloader {
 
-    suspend fun download(context: Context, url: String, filename: String): Boolean =
-        withContext(Dispatchers.IO) {
-            val newFile = File(context.filesDir, filename)
+    suspend fun download(
+        context: Context,
+        url: String,
+        filename: String
+    ): Boolean = withContext(Dispatchers.IO) {
+        val newFile = File(context.filesDir, filename)
 
-            if (newFile.exists()) {
-                return@withContext true
-            } else {
-                newFile.createNewFile()
-            }
-
-            val fullUrl = if (url.startsWith("http")) url else "https:$url"
-            var connection: HttpURLConnection? = null
-            var result = false
-
-            try {
-                connection = URL(fullUrl).openConnection() as HttpURLConnection
-                connection.connect()
-
-                val statusCode = connection.responseCode
-                if (statusCode != HttpURLConnection.HTTP_OK) {
-                    // handle error here
-                    Log.d("JIANDDEBUG", "Connection error: $statusCode")
-                    connection.disconnect()
-                    return@withContext false
-                }
-
-                connection.inputStream.use { inputStream ->
-                    newFile.outputStream().use { outputStream ->
-                        outputStream.write(inputStream.readBytes())
-                    }
-                }
-                result = true
-            } catch (e: Exception) {
-                Log.d("JIANDDEBUG", "e.message: ${e.message}")
-                e.printStackTrace()
-            } finally {
-                connection?.disconnect()
-            }
-
-            result
+        if (newFile.exists()) {
+            return@withContext true
+        } else {
+            newFile.createNewFile()
         }
+
+        val fullUrl = if (url.startsWith("http")) url else "https:$url"
+        var connection: HttpURLConnection? = null
+        var result = false
+
+        try {
+            connection = URL(fullUrl).openConnection() as HttpURLConnection
+            connection.connect()
+
+            val statusCode = connection.responseCode
+            if (statusCode != HttpURLConnection.HTTP_OK) {
+                // handle error here
+                Log.d("JIANDDEBUG", "Connection error: $statusCode")
+                connection.disconnect()
+                return@withContext false
+            }
+
+            connection.inputStream.use { inputStream ->
+                newFile.outputStream().use { outputStream ->
+                    outputStream.write(inputStream.readBytes())
+                }
+            }
+            result = true
+        } catch (e: Exception) {
+            Log.d("JIANDDEBUG", "e.message: ${e.message}")
+            e.printStackTrace()
+        } finally {
+            connection?.disconnect()
+        }
+
+        result
+    }
+
+    suspend fun downloadPdfAsImageBitmap(
+        context: Context,
+        url: String,
+        filename: String
+    ): ImageBitmap? = withContext(Dispatchers.IO) {
+        if (BitmapCache.contains(filename)) {
+            return@withContext BitmapCache[filename]
+        }
+
+        val result = download(context, url, filename)
+
+        if (result) {
+            val bitmap = PdfToBitmap.convert(context, filename)
+            return@withContext bitmap.asImageBitmap()
+        }
+
+        return@withContext null
+    }
 
 }
