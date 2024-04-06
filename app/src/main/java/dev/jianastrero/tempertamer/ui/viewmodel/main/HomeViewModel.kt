f57df4fdfc42622f461c0edbedd370e29c78bcc0
Jian James Astrero <jianjamesastrero@gmail.com>
2024-04-06 14:46:09 +0800

    🧑‍💻 Use UseCase instead of service

--- app/src/main/java/dev/jianastrero/tempertamer/ui/viewmodel/main/HomeViewModel.kt
+++ app/src/main/java/dev/jianastrero/tempertamer/ui/viewmodel/main/HomeViewModel.kt
@@ -3,7 +3,7 @@ package dev.jianastrero.tempertamer.ui.viewmodel.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dev.jianastrero.tempertamer.data.service.level.LevelService
+import dev.jianastrero.tempertamer.data.usecase.GetLevelsUseCase
 import dev.jianastrero.tempertamer.domain.entity.Level
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,13 +13,13 @@ import kotlinx.coroutines.launch
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val levelService: LevelService
+    private val getLevelsUseCase: GetLevelsUseCase
 ) : ViewModel() {
 
     private val _levels = MutableStateFlow(emptyList<Level>())
     val levels: StateFlow<List<Level>> = _levels.asStateFlow()
 
     fun fetchLevels() = viewModelScope.launch {
-        _levels.emit(levelService.getLevels())
+        _levels.emit(getLevelsUseCase())
     }
 }
